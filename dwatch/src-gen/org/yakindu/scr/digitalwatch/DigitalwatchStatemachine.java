e2735efda170b690365faea14d059bb1c6ad73ac
marko <markotaht@gmail.com>
2016-11-12 12:19:33 +0200

    Etteantud programm

---
+++ dwatch/src-gen/org/yakindu/scr/digitalwatch/DigitalwatchStatemachine.java
@@ -0,0 +1,247 @@
+package org.yakindu.scr.digitalwatch;
+
+public class DigitalwatchStatemachine implements IDigitalwatchStatemachine {
+
+	protected class SCIButtonsImpl implements SCIButtons {
+	
+		private boolean topLeftPressed;
+		
+		public void raiseTopLeftPressed() {
+			topLeftPressed = true;
+		}
+		
+		private boolean topLeftReleased;
+		
+		public void raiseTopLeftReleased() {
+			topLeftReleased = true;
+		}
+		
+		private boolean topRightPressed;
+		
+		public void raiseTopRightPressed() {
+			topRightPressed = true;
+		}
+		
+		private boolean topRightReleased;
+		
+		public void raiseTopRightReleased() {
+			topRightReleased = true;
+		}
+		
+		private boolean bottomLeftPressed;
+		
+		public void raiseBottomLeftPressed() {
+			bottomLeftPressed = true;
+		}
+		
+		private boolean bottomLeftReleased;
+		
+		public void raiseBottomLeftReleased() {
+			bottomLeftReleased = true;
+		}
+		
+		private boolean bottomRightPressed;
+		
+		public void raiseBottomRightPressed() {
+			bottomRightPressed = true;
+		}
+		
+		private boolean bottomRightReleased;
+		
+		public void raiseBottomRightReleased() {
+			bottomRightReleased = true;
+		}
+		
+		protected void clearEvents() {
+			topLeftPressed = false;
+			topLeftReleased = false;
+			topRightPressed = false;
+			topRightReleased = false;
+			bottomLeftPressed = false;
+			bottomLeftReleased = false;
+			bottomRightPressed = false;
+			bottomRightReleased = false;
+		}
+	}
+	
+	protected SCIButtonsImpl sCIButtons;
+	
+	protected class SCIDisplayImpl implements SCIDisplay {
+	
+		private SCIDisplayOperationCallback operationCallback;
+		
+		public void setSCIDisplayOperationCallback(
+				SCIDisplayOperationCallback operationCallback) {
+			this.operationCallback = operationCallback;
+		}
+	}
+	
+	protected SCIDisplayImpl sCIDisplay;
+	
+	protected class SCILogicUnitImpl implements SCILogicUnit {
+	
+		private SCILogicUnitOperationCallback operationCallback;
+		
+		public void setSCILogicUnitOperationCallback(
+				SCILogicUnitOperationCallback operationCallback) {
+			this.operationCallback = operationCallback;
+		}
+		private boolean startAlarm;
+		
+		public void raiseStartAlarm() {
+			startAlarm = true;
+		}
+		
+		protected void clearEvents() {
+			startAlarm = false;
+		}
+	}
+	
+	protected SCILogicUnitImpl sCILogicUnit;
+	
+	private boolean initialized = false;
+	
+	public enum State {
+		main_region_digitalwatch,
+		$NullState$
+	};
+	
+	private final State[] stateVector = new State[1];
+	
+	private int nextStateIndex;
+	
+	public DigitalwatchStatemachine() {
+		sCIButtons = new SCIButtonsImpl();
+		sCIDisplay = new SCIDisplayImpl();
+		sCILogicUnit = new SCILogicUnitImpl();
+	}
+	
+	public void init() {
+		this.initialized = true;
+		for (int i = 0; i < 1; i++) {
+			stateVector[i] = State.$NullState$;
+		}
+		clearEvents();
+		clearOutEvents();
+	}
+	
+	public void enter() {
+		if (!initialized) {
+			throw new IllegalStateException(
+					"The state machine needs to be initialized first by calling the init() function.");
+		}
+		enterSequence_main_region_default();
+	}
+	
+	public void exit() {
+		exitSequence_main_region();
+	}
+	
+	/**
+	 * @see IStatemachine#isActive()
+	 */
+	public boolean isActive() {
+		return stateVector[0] != State.$NullState$;
+	}
+	
+	/** 
+	* Always returns 'false' since this state machine can never become final.
+	*
+	* @see IStatemachine#isFinal()
+	*/
+	public boolean isFinal() {
+		return false;
+	}
+	/**
+	* This method resets the incoming events (time events included).
+	*/
+	protected void clearEvents() {
+		sCIButtons.clearEvents();
+		sCILogicUnit.clearEvents();
+	}
+	
+	/**
+	* This method resets the outgoing events.
+	*/
+	protected void clearOutEvents() {
+	}
+	
+	/**
+	* Returns true if the given state is currently active otherwise false.
+	*/
+	public boolean isStateActive(State state) {
+	
+		switch (state) {
+		case main_region_digitalwatch:
+			return stateVector[0] == State.main_region_digitalwatch;
+		default:
+			return false;
+		}
+	}
+	
+	public SCIButtons getSCIButtons() {
+		return sCIButtons;
+	}
+	
+	public SCIDisplay getSCIDisplay() {
+		return sCIDisplay;
+	}
+	
+	public SCILogicUnit getSCILogicUnit() {
+		return sCILogicUnit;
+	}
+	
+	/* 'default' enter sequence for state digitalwatch */
+	private void enterSequence_main_region_digitalwatch_default() {
+		nextStateIndex = 0;
+		stateVector[0] = State.main_region_digitalwatch;
+	}
+	
+	/* 'default' enter sequence for region main region */
+	private void enterSequence_main_region_default() {
+		react_main_region__entry_Default();
+	}
+	
+	/* Default exit sequence for state digitalwatch */
+	private void exitSequence_main_region_digitalwatch() {
+		nextStateIndex = 0;
+		stateVector[0] = State.$NullState$;
+	}
+	
+	/* Default exit sequence for region main region */
+	private void exitSequence_main_region() {
+		switch (stateVector[0]) {
+		case main_region_digitalwatch:
+			exitSequence_main_region_digitalwatch();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	/* The reactions of state digitalwatch. */
+	private void react_main_region_digitalwatch() {
+	}
+	
+	/* Default react sequence for initial entry  */
+	private void react_main_region__entry_Default() {
+		enterSequence_main_region_digitalwatch_default();
+	}
+	
+	public void runCycle() {
+		if (!initialized)
+			throw new IllegalStateException(
+					"The state machine needs to be initialized first by calling the init() function.");
+		clearOutEvents();
+		for (nextStateIndex = 0; nextStateIndex < stateVector.length; nextStateIndex++) {
+			switch (stateVector[nextStateIndex]) {
+			case main_region_digitalwatch:
+				react_main_region_digitalwatch();
+				break;
+			default:
+				// $NullState$
+			}
+		}
+		clearEvents();
+	}
+}
